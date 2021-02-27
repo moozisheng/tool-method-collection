7418d216a47a02e67b81ae971ba8b0f93a529143
mozisheng <809121829@qq.com>
2021-02-27 10:54:51 +0800

    feat: 实现 apply 函数

--- tools.js
+++ tools.js
@@ -32,6 +32,41 @@ Function.prototype.myCall = function(context) {
 }
 
 
+/**
+ * apply 方法也是改变执行方法当前的 this，但传递参数的格式不同，需要数组
+ * apply 做了以下事情：
+ * 1、将函数设为对象的属性
+ * 2、执行并删除这个函数
+ * 3、指定this到函数并传入给定参数执行函数
+ * 4、如果不传入参数，默认指向为 window
+ * @param {*} context 
+ * @param {*} arr 
+ */
+Function.prototype.myApply = function(context, arr) {
+    // 如果 context 不是一个对象，则默认指向 window
+    context = Object(context) || window;
+
+    //将函数设为对象的属性，通过 this 获取调用 apply 时的函数
+    context.fn = this;
+    // 定义变量缓存该函数执行的结果
+    let result;
+    if (!arr) {
+        // apply 方法没有传入第二个参数(数组)，直接执行函数
+        result = context.fn();
+    } else {
+        // apply 方法传入了第二个参数，将参数重新放入 args 中
+        const args = [];
+        for (let i = 0, len = arr.length; i < len; i++) {
+            args.push(i);
+        }
+        // 传入参数执行该函数
+        result = context.fn(...args)
+    }
+    // 删除该函数
+    delete context.fn;
+    // 返回函数执行的结果
+    return result;
+}
 
 
 
